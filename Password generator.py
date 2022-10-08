47be42ac340d49c68605737be81af9c4f35bd620
Kvnq-Poza <114484337+Kvnq-Poza@users.noreply.github.com>
2022-10-09 00:21:46 +0100

    Update Password generator.py

--- Password generator.py
+++ Password generator.py
@@ -1,8 +1,10 @@
+from string import printable
 import pyperclip, random
 from tkinter import *
 # The pyperclip module endbles copying of generated results
 # The random module enables generation of random figures
 # the tkinter module enables creation of an application window
+# the string - printable module contains values (A-Z and other characters which can be used)
 
 root = Tk()
 
@@ -13,14 +15,9 @@ passlen = IntVar() # stores the value as an Int type
 passlen.set(0) # setting initial length of the password to 0
 
 def generate():
-    password_key = ['a', 'b', 'c', 'd', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-    'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E',
-    'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
-    'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '@', '#', 
-    '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', ',', '<', '>', '?', '/', '.',
-    '|', '[', ']', '{', '}'] # key values for password generation
+    password_key = list(printable) # takes key values from the string - printable module for password generation
     password = "" # open varialbe for taking the password key
-
+    
     for p in range(passlen.get()): 
         password = password + random.choice(password_key)
     passstr.set(password) # setting the password
@@ -33,7 +30,7 @@ def clipboardcopy():
 #Label = description/direction in the GUI
 #Entry = Box for user to input values
 #Button = To pass the command and run function above
-Label(root, text = 'Password Generator', font = 'arial 27 bold', background="#F0FFFF").pack()
+Label(root, text = 'Password Generator', font = '"agency fb" 27 bold', background="#F0FFFF").pack()
 Label(root, text = 'Enter Password Length: ', background="#89CFF0").pack(pady=3)
 Entry(root, textvariable=passlen).pack(pady=3)
 Button(root, text = 'Generate Password', command=generate, background='#00FFFF').pack(pady=7)
